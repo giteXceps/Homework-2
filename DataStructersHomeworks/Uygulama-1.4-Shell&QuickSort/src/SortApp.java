465b625a469090f6760a43c95ce37acd69ee5594
Sefa Desktop <sefametindev@gmail.com>
2024-12-01 23:28:37 +0300

    DataStructers-HW1-complated

--- DataStructersHomeworks/Uygulama-1.4-Shell&QuickSort/src/SortApp.java
+++ DataStructersHomeworks/Uygulama-1.4-Shell&QuickSort/src/SortApp.java
@@ -2,8 +2,10 @@ import java.util.Random;
 
 public class SortApp {
     public static void main(String[] args) {
+
         ArraySort arraySort = new ArraySort(9);
         Random random = new Random();
+
         // rastgele sayilar ile olusan dizi
         arraySort.insert(random.nextLong(100));
         arraySort.insert(random.nextLong(100));
@@ -14,17 +16,19 @@ public class SortApp {
         arraySort.insert(random.nextLong(100));
         arraySort.insert(random.nextLong(100));
         arraySort.insert(random.nextLong(100));
-        
+
+        //dizinin normal hali yazdirilir
         System.out.println("Orijinal Dizi:");
         arraySort.display();
-        
-        // Shell Sort ile Quick Sort'u kullanarak sıralama
+
+        // Shell Sort ile sıralama
+        System.out.println("ShellSort ile siralanmis dizi:");
         arraySort.shellSort();
         arraySort.display();
-        
+
+        // Quick sort ile sırlanmis hali
+        System.out.println("QuickSort ile siralanmis dizi:");
         arraySort.quickSort();
-        
-        System.out.println("Siralanmis Dizi:");
         arraySort.display();
     }
 }
